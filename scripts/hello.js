c04b5be6614c315590ff8e9596a88edff4d81832
hibari2357 <hibari235711@gmail.com>
2019-01-27 20:04:03 +0900

    hubot brain mock

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,6 +1,13 @@
 module.exports = (robot) => {
-  robot.hear(/hello/i, (res) => {
-    console.log("heared hello");
-    res.send("World!");
+  robot.hear(/hello, world!/i, (res) => {
+    var date = new Date();
+    var hours = date.getUTCHours() + 9;
+    var minutes = date.getMinutes();
+    var perfomance = 4000*Math.exp(-(Math.max(5,hours)-5)/3);
+
+    robot.brain.set("user1", 800);
+    console.log(perfomance);
+    var after = robot.brain.get("user1");
+    console.log(after);
   })
 }
